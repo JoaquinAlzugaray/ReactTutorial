a0c12d45aef2a03c04c994d7d6d947b3727ac186
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-02-04 04:12:58 +0000

    ejercicios

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -8,17 +8,9 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * planets in html called planetsInHTML
  */
 
-const mappingFunction = (item, index) => {
-	return (
-		<li className="list-group-item" key={index}>
-			{item}
-		</li>
-	);
-};
 
-const planetsInHTML = planets.map(mappingFunction);
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = <ul className="list-group m-5">{planetsInHTML}</ul>;
+const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- .learn/resets/06-listening-to-events/app.jsx
+++ .learn/resets/06-listening-to-events/app.jsx
@@ -2,22 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const clickHandler = (e) => {
+const clickHandler = e => {
 	console.log("I was clicked!", e);
 };
 
-const Alert = (props) => {
-	return (
-		<button className="btn btn-success btn-lg" onClick={clickHandler}>
-			{props.text}
-		</button>
-	);
-};
-
-Alert.propTypes = {
-	text: PropTypes.string,
+const Alert = props => {
+	return <button className="btn btn-success btn-lg">Click Me</button>;
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understand what properties is the component using
-ReactDOM.render(<Alert text="Click me" />, document.querySelector("#myDiv"));
+ReactDOM.render(<Alert />, document.querySelector("#myDiv"));
